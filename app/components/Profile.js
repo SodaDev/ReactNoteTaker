548a90a7a917933ca8e7aea2629cfde642a191fd
SodaDev <soda@Marcins-MacBook-Pro.local>
2015-10-07 19:04:28 +0200

    Moving to ES6

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,53 +1,59 @@
-var React = require('react');
-var Router = require('react-router');
-var ReactFireMixin = require('reactfire');
-var Firebase = require('firebase');
-var helpers = require('../utils/helpers');
+import React from 'react';
+import Router from 'react-router';
+import Repos from '../Github/Repos';
+import UserProfile from '../Github/UserProfile';
+import Notes from '../Notes/Notes';
+import helpers from '../utils/helpers';
+import Rebase from 're-base';
 
-var UserProfile = require('../Github/UserProfile');
-var Repos = require('../Github/Repos');
-var Notes = require('../Notes/Notes');
+var base = Rebase.createClass('https://fiery-inferno-9358.firebaseio.com/');
 
-var Profile = React.createClass({
-    mixins: [Router.State, ReactFireMixin],
-    getInitialState: function(){
-        return {
+class Profile extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
             notes: [],
             bio: {
                 name: ''
             },
             repos: []
         }
-    },
-    init: function(){
-        var childRef = this.ref.child(this.getParams().username);
-        this.bindAsArray(childRef, "notes");
+    }
+    init(){
+        this.ref = base.bindToState(this.router.getCurrentParams().username, {
+            context: this,
+            asArray: true,
+            state: 'notes'
+        });
 
-        helpers.getGithubInfo(this.getParams().username)
-            .then(function(dataObj){
-                this.setState({
-                    bio: dataObj.bio,
-                    repos: dataObj.repos
-                })
-            }.bind(this));
-    },
-    componentDidMount: function(){
-        this.ref = new Firebase("https://fiery-inferno-9358.firebaseio.com/");
+        helpers.getGithubInfo(this.router.getCurrentParams().username)
+        .then((dataObj) => {
+            this.setState({
+                bio: dataObj.bio,
+                repos: dataObj.repos
+            })
+        });
+    }
+    componentDidMount(){
         this.init();
-    },
-    componentWillUnmount: function(){
-        this.unbind('notes');
-    },
-    componentWillReceiveProps: function(){
-        this.unbind('notes');
+    }
+    componentWillMount(){
+        this.router = this.context.router;
+    }
+    componentWillUnmount(){
+        base.removeBinding(this.ref);
+    }
+    componentWillReceiveProps(){
+        base.removeBinding(this.ref);
         this.init();
-    },
-    handleAddNote: function(newNote){
-        this.ref.child(this.getParams().username)
-                .set(this.state.notes.concat([newNote]));
-    },
-    render: function(){
-        var username = this.getParams().username;
+    }
+    handleAddNote(newNote){
+        base.post(this.router.getCurrentParams().username, {
+            data: this.state.notes.concat([newNote])
+        })
+    }
+    render(){
+        var username = this.router.getCurrentParams().username;
 
         return (
             <div className="row">
@@ -60,11 +66,15 @@ var Profile = React.createClass({
                 <div className="col-md-4">
                     <Notes username={username}
                            notes={this.state.notes}
-                           addNote={this.handleAddNote} />
+                           addNote={this.handleAddNote.bind(this)} />
                 </div>
             </div>
         );
     }
-});
+}
 
-module.exports = Profile;
+Profile.contextTypes = {
+    router: React.PropTypes.func.isRequired
+};
+
+export default Profile;
--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,24 +1,26 @@
-var axios = require('axios');
+import axios from 'axios';
 
 function getRepos(username){
-    return axios.get('https://api.github.com/users/' + username + '/repos');
+    return axios.get(`https://api.github.com/users/${username}/repos`);
 }
 
 function getUserInfo(username){
-    return axios.get('https://api.github.com/users/' + username);
+    return axios.get(`https://api.github.com/users/${username}`);
 }
 
-module.exports = {
-    getGithubInfo: function (username) {
+var helpers = {
+    getGithubInfo(username) {
         return axios.all([getRepos(username), getUserInfo(username)])
-            .then(function (response) {
+            .then((response) => {
                 return {
                     repos: response[0].data,
                     bio: response[1].data
                 };
-            }, function(error){
+            }, (error) => {
                 console.error('There is no user ' + username)
                 console.log(error);
             });
     }
 };
+
+export default helpers;
